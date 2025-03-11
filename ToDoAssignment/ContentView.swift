cb65ba5c90bc46f63f9c7b9bc4f44a73de67ace3
tanmayxx12 <troy12rollsza@gmail.com>
2025-03-11 15:11:00 +0530

    Initial Commit

---
+++ ToDoAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoAssignment
+//
+//  Created by Tanmay . on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoAssignment/ToDoAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAssignmentApp.swift
+//  ToDoAssignment
+//
+//  Created by Tanmay . on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
